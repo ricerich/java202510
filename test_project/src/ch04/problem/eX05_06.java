f58870382ce79a1e3755033dc383093596f1caa0
it <it@DESKTOP-7RKLJFI>
2025-11-17 16:30:16 +0900

    6번 풀이

--- test_project/src/ch04/problem/eX05_06.java
+++ test_project/src/ch04/problem/eX05_06.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 class Circle
 {
-	
 	private double x, y;	
 	private int radius;
 	
@@ -18,6 +17,11 @@ class Circle
 	{
 		System.out.println("("+x+","+y+")"+radius);
 	}
+	
+	public double getArea()
+	{
+		return radius * radius * 3.14;
+	}
 			
 }
 
@@ -25,6 +29,7 @@ class CircleManager
 {
 	void run() {
 		Scanner sc1 = new Scanner(System.in);
+		
 		Circle c[] = new Circle[3];
 		for(int i=0; i<c.length;i++) 
 		{
@@ -41,10 +46,35 @@ class CircleManager
 			c[i].show();
 		}
 		
+		//1.max가 있는 버전
+//		double max=0;
+//		int max_idx=0;
+//		for(int i=0; i<c.length;i++) 
+//		{
+//			if(max < c[i].getArea()) {
+//				max = c[i].getArea();//최대값을 갱신
+//				max_idx = i; 
+//			}
+//		}
+		
+		//2.max없이 max_idx만 가지고 푸는 버전
+//		double max=0;//없어도 됨!
+		int max_idx=0;
+		for(int i=0; i<c.length;i++) 
+		{
+			if(c[max_idx].getArea() < c[i].getArea()) {
+//				max = c[i].getArea();//최대값을 갱신//없어도 됨!
+				max_idx = i; 
+			}
+		}
+		
+		System.out.println("가장 면적인 큰 원은?");
+		//(3,4)6인 원입니다....
+		c[max_idx].show();
 	}
 }
 
-public class eX05 {
+public class eX05_06 {
 
 	public static void main(String[] args) 
 	{
